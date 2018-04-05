bc767adc6a7ea6cf3e4af8fecde6cd53b6b3395c
David Paul <enlightenedpie@users.noreply.github.com>
2018-04-04 17:58:39 -0700

    Initial modifications

---
+++ web/app/themes/api/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html class="no-js" <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
