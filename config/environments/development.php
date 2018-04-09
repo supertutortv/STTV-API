a4083a2d957a8b7d63ed0b718079ce8972045215
David Paul <enlightenedpie@users.noreply.github.com>
2018-04-09 13:21:10 -0700

    Added environment API keys

--- config/environments/development.php
+++ config/environments/development.php
@@ -3,3 +3,8 @@
 define('SAVEQUERIES', true);
 define('WP_DEBUG', true);
 define('SCRIPT_DEBUG', true);
+
+/** Stripe Keys */
+define('STRIPE_PK', env('STRIPE_PK_TEST'));
+define('STRIPE_SK', env('STRIPE_SK_TEST'));
+define('STRIPE_WHSEC', env('STRIPE_WHSEC_TEST'));
--- config/environments/staging.php
+++ config/environments/staging.php
@@ -5,3 +5,8 @@ define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
 /** Disable all file modifications including updates and update notifications */
 define('DISALLOW_FILE_MODS', true);
+
+/** Stripe Keys */
+define('STRIPE_PK', env('STRIPE_PK_LIVE'));
+define('STRIPE_SK', env('STRIPE_SK_LIVE'));
+define('STRIPE_WHSEC', env('STRIPE_WHSEC_LIVE'));
