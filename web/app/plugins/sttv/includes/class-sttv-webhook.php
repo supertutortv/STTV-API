4225b89a7b166cd5a8551c56a83b093c0f72aa18
David Paul <enlightenedpie@users.noreply.github.com>
2018-04-09 09:11:52 -0700

    Added Webhook class

---
+++ web/app/plugins/sttv/includes/class-sttv-webhook.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace STTV;
+
+if ( ! defined( 'ABSPATH' ) ) {exit;}
+
+class Webhook {
+
+    public static function init() {
+
+    }
+
+    public static function respond( $data ) {
+        //change event dot notation to underscores
+        $event = str_replace( '.', '_', $data['event'] );
+
+        $response = ( is_callable( $event ) ) ? self::log( $event( $data ), 'receive' ) : 'Valid webhook, but no action taken. Thank you!';
+
+        http_response_code(200);
+		echo wp_send_json( $response );
+
+    }
+
+    public static function send( $body ) {
+        return self::log( wp_safe_remote_request( '', 
+            [
+
+            ]
+        ) );
+    }
+
+    private static function verifySignature( $sig ) {
+        return ( $sig === hash_hmac( 'sha256', $body, $sec ) );
+    }
+
+    private static function sign( $payload ) {
+        return hash_hmac( 'sha256', $payload, env( 'WHSEC_TOKEN' ) );
+    }
+
+    private static function log( $response, $type = 'send' ) {
+        return $response;
+    }
+}
