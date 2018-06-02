07f17e85bb3172fc415b21d883004bf59242602d
David Paul <enlightenedpie@users.noreply.github.com>
2018-06-02 15:33:08 -0700

    cron upd

--- web/app/plugins/sttv/includes/sttv-cron.php
+++ web/app/plugins/sttv/includes/sttv-cron.php
@@ -5,6 +5,10 @@ use Vimeo\Vimeo;
 
 class Cron {
 
+    private $tests = [
+        'act'
+    ];
+
     private $vtok = '57e52c4bb16997b539ebed506a099c36';
 
     private $vsec = '8ZwrHod1K6aec7ZAPETVPgcshokYimu9Jt9Ms+zOfvFetzogUlNHAOvPWm1Emn5iw2BrVuEl/UyCnt/gny6W1iJKadmqDXGTR/PgFj25p/2t65uZdVkJEBsxxs/ZkUf0';
@@ -22,9 +26,11 @@ class Cron {
     public function vcache() {
         try {
             $vimeo = new Vimeo( $this->vclient, $this->vsec, $this->vtok );
-            $alb_data = $vimeo->request( '/me/albums?query=ACT&fields=uri,name&per_page=100' );
-            $albs = (array) $alb_data['body']['data'];
-            print_r( $albs );
+            foreach ( $this->tests as $test ) {
+                $alb_data = $vimeo->request( "/me/albums?query=$test&fields=uri,name&per_page=100" );
+                $albs = (array) $alb_data['body']['data'];
+                print_r( $albs );
+            }
         } catch ( Exception $e ) {
             print_r( $e );
         }
