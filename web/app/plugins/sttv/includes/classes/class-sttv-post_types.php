3e971a5448c1f2ce7c0d4a1c90433f5df3ca3031
David Paul <enlightenedpie@users.noreply.github.com>
2018-06-02 10:19:37 -0700

    upd

--- web/app/plugins/sttv/includes/classes/class-sttv-post_types.php
+++ web/app/plugins/sttv/includes/classes/class-sttv-post_types.php
@@ -9,7 +9,7 @@ class Post_Types {
     public static function init() {
         add_action( 'init', [ __CLASS__, 'register_post_types' ], 5 );
         add_action( 'add_meta_boxes', [ __CLASS__, 'add_meta_boxes' ] );
-        add_filter( 'upload_dir', 'course_resource_upload' );
+        add_filter( 'upload_dir', [ __CLASS__, 'course_resource_upload' ] );
     }
 
     public static function register_post_types() {
