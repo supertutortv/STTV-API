04e345fdee64c31404d5bc2a25df17ae2deb6329
David Paul <enlightenedpie@users.noreply.github.com>
2018-06-02 15:25:59 -0700

    Cron upd

--- web/app/plugins/sttv/includes/sttv-cron.php
+++ web/app/plugins/sttv/includes/sttv-cron.php
@@ -1,6 +1,15 @@
 <?php
 
+require_once __DIR__ . '/vimeo/autoload.php';
+use Vimeo\Vimeo;
+
 class Cron {
+
+    private $vtok = '57e52c4bb16997b539ebed506a099c36';
+
+    private $vsec = '8ZwrHod1K6aec7ZAPETVPgcshokYimu9Jt9Ms+zOfvFetzogUlNHAOvPWm1Emn5iw2BrVuEl/UyCnt/gny6W1iJKadmqDXGTR/PgFj25p/2t65uZdVkJEBsxxs/ZkUf0';
+
+    private $vclient = '5fc7293a02bf8a93179503d7bf72fb190cf8e9af';
 
     public function __construct( $method ) {
         $this->$method();
@@ -11,7 +20,14 @@ class Cron {
     }
 
     public function vcache() {
-        echo 'Hello World!';
+        try {
+            $vimeo = new Vimeo( $this->vclient, $this->vsec, $this->vtok );
+            $alb_data = $vimeo->request( '/me/albums?query=ACT&fields=uri,name&per_page=100' );
+            $albs = (array) $alb_data['body']['data'];
+            print_r( $albs );
+        } catch ( Exception $e ) {
+            print_r( $e );
+        }
     }
 
 }
