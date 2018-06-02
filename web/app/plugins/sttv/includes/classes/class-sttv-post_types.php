2976ccdc255aa31c7b27e49860f8358e221d6aa9
David Paul <enlightenedpie@users.noreply.github.com>
2018-06-02 10:14:01 -0700

    upd

--- web/app/plugins/sttv/includes/classes/class-sttv-post_types.php
+++ web/app/plugins/sttv/includes/classes/class-sttv-post_types.php
@@ -9,6 +9,7 @@ class Post_Types {
     public static function init() {
         add_action( 'init', [ __CLASS__, 'register_post_types' ], 5 );
         add_action( 'add_meta_boxes', [ __CLASS__, 'add_meta_boxes' ] );
+        add_filter( 'upload_dir', 'course_resource_upload' );
     }
 
     public static function register_post_types() {
@@ -72,56 +73,12 @@ class Post_Types {
              'normal', // $context
              'low' // $priority
         );
-    
-        /* add_meta_box(
-             'course_product_page', // $id
-             'Course Product Page', // $title
-             [ __CLASS__ , 'sttv_display_course_product_page' ], // $callback
-             'courses', // $post_type
-             'side', // $context
-             'low' // $priority
-        );
-
-        add_meta_box(
-            'course_introvid_album', // $id
-            'Course Introvideo Album', // $title
-            [ __CLASS__ , 'sttv_display_course_introvid_album' ], // $callback
-            'courses', // $post_type
-            'side', // $context
-            'low' // $priority
-       ); */
     }
 
-    public static function sttv_display_course_product_page() { ?>
-		<select name="product_page_dropdown">
-            <option value=""><?php echo esc_attr( __( 'Select page' ) ); ?></option> 
-		<?php global $post;
-		    $cpp = get_post_meta( $post->ID, 'course_product_page', true );
-		    $pages = get_pages(); 
-            foreach ( $pages as $page ) :
-                $selected = ($page->ID == $cpp) ? 'selected' : '';
-                $option = '<option value="' . $page->ID . '" '.$selected.'>';
-                $option .= $page->post_title;
-                $option .= '</option>';
-                echo $option;
-            endforeach;
-		?>
-		</select>
-	<?php 
-		wp_nonce_field( 'save_course_product_page_nonce', 'course_product_page_nonce' );										   
-	}
-	
-	public static function sttv_display_course_introvid_album() { 
-		global $post;
-		?>
-		<input type="text" name="course_introvid_album" value="<?php echo get_post_meta( $post->ID, 'course_introvid_album', true ); ?>"/>
-    <?php }
-    
-    public static function sttv_display_course_meta() {
+    public static function course_resource_upload( $args ) {
         global $post;
-        $fields = get_fields( $post->ID ); ?>
-        <pre><?php print_r( $fields ); ?></pre>
-    <?php }
+        return $post->ID;
+    }
 
 }
 Post_Types::init();
