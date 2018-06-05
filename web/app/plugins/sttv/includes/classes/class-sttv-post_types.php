4cd021ba1ace2073a880bca73219beca09cafd0f
David Paul <enlightenedpie@users.noreply.github.com>
2018-06-05 12:25:33 -0700

    upd

--- web/app/plugins/sttv/includes/classes/class-sttv-post_types.php
+++ web/app/plugins/sttv/includes/classes/class-sttv-post_types.php
@@ -77,8 +77,8 @@ class Post_Types {
     public static function sttv_display_course_meta() {
         //global $post, $wp_rewrite;
         //$fields = get_fields( $post->ID ); 
-        //$meta = json_encode( get_post_meta( $post->ID, 'sttv_course_data', true ), JSON_PRETTY_PRINT ); ?>
-        <pre><?php //print_r( $meta ); ?></pre>
+        $meta = json_encode( get_post_meta( $post->ID, 'sttv_course_data', true ), JSON_PRETTY_PRINT ); ?>
+        <pre><?php print_r( $meta ); ?></pre>
     <?php }
 
 }
