b8d542946540333013292429996358120ec19b59
David Paul <enlightenedpie@users.noreply.github.com>
2018-06-07 13:28:47 -0700

    upd

--- web/app/plugins/sttv/includes/classes/checkout/class-sttv-checkout-charge.php
+++ web/app/plugins/sttv/includes/classes/checkout/class-sttv-checkout-charge.php
@@ -4,5 +4,15 @@ namespace STTV\Checkout;
 defined( 'ABSPATH' ) || exit;
 
 class Charge extends Stripe {
+
+    public function save() {
+        global $wpdb;
+        $res = $this->response;
+    }
+
+    protected function sanitize( $obj ) {
+        $sanitized = $obj;
+        return $sanitized;
+    }
     
 }
