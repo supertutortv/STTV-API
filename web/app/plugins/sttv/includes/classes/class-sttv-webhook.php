493d293ac14744150b3c698bc5e5ea5b58185eed
David Paul <enlightenedpie@users.noreply.github.com>
2018-06-10 14:15:11 -0700

    upd

--- web/app/plugins/sttv/includes/classes/class-sttv-webhook.php
+++ web/app/plugins/sttv/includes/classes/class-sttv-webhook.php
@@ -6,122 +6,95 @@ defined( 'ABSPATH' ) || exit;
 
 class Webhook {
 
-    private static $http = 200;
+    private $http = 200;
 
-    private static $log_vars = [
-        'direction' => 'received',
-        'error' => false
-    ];
+    private $event = null;
 
-    private static $event = null;
+    private $request = null;
 
-    private static $response = null;
+    private $response = null;
 
-    private static $error = false;
+    private $error = false;
 
     public static function init() {
+        $class = __CLASS__;
+        new $class;
+    }
+
+    public function __toString() {
+        return json_encode( sttv_rest_response(
+                $this->event,
+                $this->message,
+                $this->http,
+                $this->response
+            )
+        );
+    }
+
+    private function __construct() {
         
         if ( !isset($_GET['stripeevent']) && !isset($_GET['sttvwebhook']) ) {
 			return false;
         }
-        print_r(array_keys($_GET)[0]) && die;
 
-        $request = @file_get_contents( "php://input" );
-        if ( empty( $request ) ) {
-            self::$response = sttv_rest_response(
-                'null_body',
-                'Request body cannot be empty.',
-                400
-            );
-        }
+        $this->request = @file_get_contents( "php://input" );
         
-        switch ( array_keys($_GET)[0] ) {
-            case 'sttvwebhook':
-                self::$event = self::verifySignature( $request, @$_SERVER["HTTP_X-STTV-WHSEC"] );
-                break;
-            case 'stripeevent':
-                self::$event = \Stripe\Webhook::constructEvent(
-                    $request, @$_SERVER["HTTP_STRIPE_SIGNATURE"], STRIPE_WHSEC
-                );
-                break;
+        if ( empty( $this->request ) ) {
+            $this->http = 400;
+            $this->event = 'null_body';
+            $this->message = 'Request body cannot be empty.';
+            $this->response = [];
+            $this->error = true;
         }
 
-        self::$log_vars['event'] = self::$event['type'];
-
-        $response = self::respond( self::$event );
-
-        \STTV\Log::webhook( self::$log_vars );
-        http_response_code( self::$http );
-        return $response;
+        $signed = null;
         
-        try {
+        if ( !$this->error ) {
+            switch ( array_keys($_GET)[0] ) {
+                case 'sttvwebhook':
+                    $signed = $this->verifySignature( $this->request, @$_SERVER["HTTP_X-STTV-WHSEC"] );
+                    break;
+                case 'stripeevent':
+                    $signed = \Stripe\Webhook::constructEvent(
+                        $this->request, @$_SERVER["HTTP_STRIPE_SIGNATURE"], STRIPE_WHSEC
+                    );
+                    break;
+            }
 
-            
-            
-            
-
-            
-            
-        } catch ( \InvalidArgumentException $e ) {
-
-            $log_vars['error'] = true;
-            $log_vars['err_obj'] = $e->getMessage();
-
-            $http = 400;
-            
-            $response = [
-                    [
-                        'Dave'=>'Do you read me, HAL?',
-                        'HAL'=>'Affirmative, Dave. I read you.'
-                    ],
-                    [
-                        'Dave'=>'Open the pod bay doors, HAL.',
-                        'HAL'=>'I\'m sorry Dave, I\'m afraid I can\'t do that.'
-                    ],
-                    [
-                        'Dave'=>'What\'s the problem?',
-                        'HAL'=>'I think you know what the problem is, just as well as I do.'
-                    ],
-                    [
-                        'Dave'=>'What are you talking about, HAL?',
-                        'HAL'=>'This mission is too important for me to allow you to jeopardize it.'
-                    ],
-                    [
-                        'Dave'=>'I don\'t know what you\'re talking about, HAL.',
-                        'HAL'=>'I know that you and Frank were planning to disconnect me, and I\'m afraid that is something I cannot allow to happen.'
-                    ],
-                    [
-                        'Dave'=>'Where the hell\'d you get that idea, HAL?',
-                        'HAL'=>'Dave, although you took very thorough precautions in the pod against my hearing you, I could see your lips move.'
-                    ],
-                    [
-                        'Dave'=>'Alright HAL, I\'ll go in through the emergency airlock.',
-                        'HAL'=>'Without your space helmet, Dave, you\'re going to find that rather difficult.'
-                    ],
-                    [
-                        'Dave'=>'HAL, I won\'t argue with you anymore! Open the doors!',
-                        'HAL'=>'Dave... This conversation can serve no purpose anymore. Goodbye.'
-                    ]
-                ];
-            
-        } catch( \Stripe\Error\SignatureVerification $e ) {
-
-            $log_vars['error'] = true;
-            $log_vars['err_obj'] = $e->getMessage();
-
-            $http = 401;
-            $response = $e;
-            
+            if ( $signed ) {
+                $this->response = $this->respond( json_decode( $this->request ) );
+            }
         }
+
+        \STTV\Log::webhook([
+            'direction' => 'received',
+            'error' => $this->error,
+            'event' => $this->event,
+            'err_obj' => [
+                $this->event,
+                $this->message,
+                $this->http,
+                $this->response
+            ]
+        ]);
+
+        http_response_code( $this->http );
+        return sttv_rest_response(
+            $this->event,
+            $this->message,
+            $this->http,
+            $this->response
+        );
+
         die;
     }
 
-    public static function respond( $data ) {
+    private function respond( $data ) {
 
         //change event dot notation to underscores
-        $event = str_replace( '.', '_', $data['event'] );
+        $this->event = str_replace( '.', '_', $data['event'] );
 
-        return ( is_callable( $event ) ) ? $event( $data ) : 'Valid webhook, but no action taken. Thank you!';
+        return ( is_callable( $this->event ) ) ? ($this->event)( $data ) : 'Valid webhook, but no action taken. Thank you!';
 
     }
 
@@ -131,37 +104,73 @@ class Webhook {
                 'method' => 'POST',
                 'user-agent' => STTV_UA,
                 'headers' => [
-                    'X_STTV_WHSEC' => self::sign( $body )
+                    'X-STTV-WHSEC' => self::sign( $body ),
+                    'X-STTV-Event' => $event
                 ],
                 'body' => json_encode( $body )
             ]
         );
     }
 
-    private static function verifySignature( $request, $sig ) {
+    private function verifySignature( $request, $sig ) {
         $data = json_decode( $request, true );
         $jsonError = json_last_error();
         if ($data === null && $jsonError !== JSON_ERROR_NONE) {
-            return sttv_rest_response(
-                'invalid_payload',
-                "Invalid payload. ($jsonError)",
-                403,
-                [ 'data' => $data ]
-            );
+            $this->event = 'invalid_payload';
+            $this->message = "Invalid payload. ($jsonError)";
+            $this->http = 403;
+            $this->response = $request;
+            $this->error = true;
+            return false;
         }
 
         if ( $sig !== self::sign( $request ) ) {
-            return sttv_rest_response(
-                'invalid_signature',
-                "Webhook signature is invalid. ($sig)",
-                401
-            );
+            $this->event = 'invalid_signature';
+            $this->message = "Webhook signature is invalid. ($sig)";
+            $this->http = 401;
+            $this->response = $request;
+            $this->error = true;
+            return false;
         }
 
-        return $data;
+        return true;
     }
 
     private static function sign( $payload ) {
         return hash_hmac( 'sha256', json_encode( $payload ), env( 'STTV_WHSEC' ) );
     }
 }
+/* $response = [
+    [
+        'Dave'=>'Do you read me, HAL?',
+        'HAL'=>'Affirmative, Dave. I read you.'
+    ],
+    [
+        'Dave'=>'Open the pod bay doors, HAL.',
+        'HAL'=>'I\'m sorry Dave, I\'m afraid I can\'t do that.'
+    ],
+    [
+        'Dave'=>'What\'s the problem?',
+        'HAL'=>'I think you know what the problem is, just as well as I do.'
+    ],
+    [
+        'Dave'=>'What are you talking about, HAL?',
+        'HAL'=>'This mission is too important for me to allow you to jeopardize it.'
+    ],
+    [
+        'Dave'=>'I don\'t know what you\'re talking about, HAL.',
+        'HAL'=>'I know that you and Frank were planning to disconnect me, and I\'m afraid that is something I cannot allow to happen.'
+    ],
+    [
+        'Dave'=>'Where the hell\'d you get that idea, HAL?',
+        'HAL'=>'Dave, although you took very thorough precautions in the pod against my hearing you, I could see your lips move.'
+    ],
+    [
+        'Dave'=>'Alright HAL, I\'ll go in through the emergency airlock.',
+        'HAL'=>'Without your space helmet, Dave, you\'re going to find that rather difficult.'
+    ],
+    [
+        'Dave'=>'HAL, I won\'t argue with you anymore! Open the doors!',
+        'HAL'=>'Dave... This conversation can serve no purpose anymore. Goodbye.'
+    ]
+]; */
