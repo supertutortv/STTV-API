14fd5ff9b22cf389a21e006ab7d981c6a74f42a9
David Paul <enlightenedpie@users.noreply.github.com>
2018-06-11 12:28:42 -0700

    upd

--- web/app/plugins/sttv/includes/class-sttv.php
+++ web/app/plugins/sttv/includes/class-sttv.php
@@ -104,7 +104,6 @@ final class STTV {
 
         add_action( 'sttv_loaded', [ $this, 'sttv_loaded' ], 999 );
         add_action( 'print_test', function() {
-           /*  global $wpdb;
             $test_order = new \STTV\Checkout\Order( 'create', [
                 'customer' => 'cus_CUDuy8TMMclqZs',
                 'trial' => 5,
@@ -135,7 +134,7 @@ final class STTV {
             $order = $test_order->response();
             //$pay = $order->pay();
             //$order = new \STTV\Checkout\Order( 'retrieve', 'in_1CaXUYIdKWhsvVLLLHBmIo4X' );
-            print_r( $order ); */
+            print_r( $order );
         });
     }
 
