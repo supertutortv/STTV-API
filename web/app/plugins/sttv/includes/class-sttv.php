db43f0e95f7da8b0ae3875b38d63ab3809549809
David Paul <enlightenedpie@users.noreply.github.com>
2018-06-13 16:38:45 -0700

    afvfv

--- web/app/plugins/sttv/includes/class-sttv.php
+++ web/app/plugins/sttv/includes/class-sttv.php
@@ -135,7 +135,7 @@ final class STTV {
             //$pay = $order->pay();
             //$order = new \STTV\Checkout\Order( 'retrieve', 'in_1CaXUYIdKWhsvVLLLHBmIo4X' );
             print_r( $order ); */
-            print_r( apply_filters( 'rest_url' ) );
+            print_r( get_rest_url() );
         });
     }
 
