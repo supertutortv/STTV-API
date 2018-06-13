2487c32adde5b2301b1ced6b570b8a4f2b087346
David Paul <enlightenedpie@users.noreply.github.com>
2018-06-13 15:39:06 -0700

    qwvwdc
    wrwd

--- web/app/plugins/sttv/includes/classes/class-sttv-logger.php
+++ web/app/plugins/sttv/includes/classes/class-sttv-logger.php
@@ -19,7 +19,7 @@ class Log {
             }
             
             $name = substr( $file, 0, -4 );
-            if ( strtotime( $name ) + (DAY_IN_SECONDS * 7) < strtotime( date('m-d-Y') ) ) {
+            if ( strtotime( $name ) < strtotime( date('m-d-Y') ) - (DAY_IN_SECONDS * 7) ) {
                 $f[] = $name;
                 //unlink( $dir . '/' . $file );
             }
