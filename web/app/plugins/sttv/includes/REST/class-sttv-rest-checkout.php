8c6b8f56b4959ab58f5d314c6c330697c7b8ee05
David Paul <enlightenedpie@users.noreply.github.com>
2018-06-14 13:24:11 -0700

    crceqrc

--- web/app/plugins/sttv/includes/REST/class-sttv-rest-checkout.php
+++ web/app/plugins/sttv/includes/REST/class-sttv-rest-checkout.php
@@ -283,7 +283,7 @@ class Checkout extends \WP_REST_Controller {
 
             $order = new \STTV\Checkout\Order( 'create', [
                 'customer' => $customer['id'],
-                'trial' => $body['trial'] ? $course['pricing']['trial_period'] : 0,
+                'trial' => ($body['trial']) ? $course['pricing']['trial_period'] : 0,
                 'metadata' => [
                     'wp_id' => $user_id,
                     'course' => $course['id'],
