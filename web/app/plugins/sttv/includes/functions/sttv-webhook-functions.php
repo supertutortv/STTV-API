eddc8a503bc141c3e176a316406508802880b8d6
David Paul <enlightenedpie@users.noreply.github.com>
2018-06-15 11:10:06 -0700

    wthgrw

--- web/app/plugins/sttv/includes/functions/sttv-webhook-functions.php
+++ web/app/plugins/sttv/includes/functions/sttv-webhook-functions.php
@@ -102,7 +102,7 @@ function invoice_created( $data ) {
         [
             'invoice_id' => $obj['id'],
             'wp_id' => $obj['metadata']['wp_id'] ?? 1,
-            'exp_date' => $obj['due_date']
+            'exp_date' => $obj['due_date'] + 3
         ],
         [
             '%s',
