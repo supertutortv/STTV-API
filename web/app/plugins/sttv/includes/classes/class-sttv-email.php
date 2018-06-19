0a6e2ac8421dd896e3f281016c52503ea381f787
David Paul <enlightenedpie@users.noreply.github.com>
2018-06-19 14:53:59 -0700

    qrevewvef

--- web/app/plugins/sttv/includes/classes/class-sttv-email.php
+++ web/app/plugins/sttv/includes/classes/class-sttv-email.php
@@ -6,11 +6,15 @@ defined( 'ABSPATH' ) || exit;
 
 class Email {
 
+    private static $email_sent = false;
+
     private $to;
 
-    private $subject = 'Default subject';
+    private $subject;
 
-    private $message = 'Default message';
+    private $message;
+
+    private $headers;
 
     public function __construct( $args=[] ) {
         $this->to = $args['to'] ?? get_option('admin_email');
@@ -22,7 +26,24 @@ class Email {
         return $this;
     }
 
+    public function send() {
+        $params = $this->get_params();
+
+        self::$email_sent = $this->_send();
+        return $this;
+    }
+
     public function get_params() {
         return get_object_vars($this);
+    }
+
+    public function email_sent() {
+        return self::$email_sent;
+    }
+
+    private function _send() {
+        return wp_mail(
+            $this->to
+        );
     }
 }
