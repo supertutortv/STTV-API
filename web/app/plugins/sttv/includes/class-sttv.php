1a1ecc9d0c78731ad38030aec08dfcad80ad76d3
David Paul <enlightenedpie@users.noreply.github.com>
2018-06-19 14:58:35 -0700

    vfaevbebv

--- web/app/plugins/sttv/includes/class-sttv.php
+++ web/app/plugins/sttv/includes/class-sttv.php
@@ -105,7 +105,7 @@ final class STTV {
 
         add_action( 'sttv_loaded', [ $this, 'sttv_loaded' ], 999 );
         add_action( 'print_test', function() {
-            print_r((new Email())->send());
+            print_r((new Email())->send()->get_params());
         });
     }
 
