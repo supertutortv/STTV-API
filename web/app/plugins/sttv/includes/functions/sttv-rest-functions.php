7151bfc328552ce55f59332794bf8c3d611a5ba6
David Paul <enlightenedpie@users.noreply.github.com>
2018-06-29 10:44:43 -0700

    vqtvqtbv

--- web/app/plugins/sttv/includes/functions/sttv-rest-functions.php
+++ web/app/plugins/sttv/includes/functions/sttv-rest-functions.php
@@ -23,10 +23,9 @@ function sttv_rest_response( $code = '', $msg = '', $status = 200, $extra = [] )
 function sttv_verify_web_token( WP_REST_Request $request ) {
     $token = new \STTV\JWT( $request->get_header('Authorization') );
     if ( $token->error !== false ) return $token->error;
-    print_r($token->payload->sub);
-    return;
 
     $pieces = explode( '|', $token->payload->sub );
+    print_r($pieces);
     list( $email, $id ) = $pieces;
 
     $user = wp_set_current_user( $id );
