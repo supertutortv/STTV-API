134c4aef53ef8795ea5cceab65c7eb8058b61995
David Paul <enlightenedpie@users.noreply.github.com>
2018-07-02 12:00:07 -0700

    qvvev

--- web/app/plugins/sttv/includes/REST/class-sttv-rest-courses.php
+++ web/app/plugins/sttv/includes/REST/class-sttv-rest-courses.php
@@ -64,6 +64,7 @@ class Courses extends \WP_REST_Controller {
 		global $wpdb;
 		$userid = get_current_user_id();
 		$umeta = get_user_meta( $userid, 'sttv_user_data', true );
+		return $umeta;
 
 		if ( !isset( $umeta['courses'] ) ) return sttv_rest_response(
 			'course_data_invalid',
@@ -199,6 +200,13 @@ class Courses extends \WP_REST_Controller {
 				if ( isset( $body['default_course'] ) ) {
 					$umeta['user']['settings']['default_course'] = $updated['default_course'] = sanitize_title_with_dashes($body['default_course']);
 				}
+				$umeta['courses'] = ['the-best-act-prep-course-ever'=>[]];
+				$umeta['user']['data'] = [
+					'customer' => 'cus_D3tCIHbTmv09C8',
+					'uid' => '',
+					'orders' => [],
+					'login_timestamps' => []
+				]; 
 				update_user_meta( $userid, 'sttv_user_data', $umeta );
 				break;
 			default:
