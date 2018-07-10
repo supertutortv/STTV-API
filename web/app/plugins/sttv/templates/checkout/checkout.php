9f452f0b123aa39a2cbb1d5034f648314d3f2967
David Paul <enlightenedpie@users.noreply.github.com>
2018-07-09 20:09:13 -0700

    twbwrtbrtb

--- web/app/plugins/sttv/templates/checkout/checkout.php
+++ web/app/plugins/sttv/templates/checkout/checkout.php
@@ -126,13 +126,13 @@ $ck4 = <<<HTML
                 <div class="col s2 right-align">Price</div>
             </div>
             <div class="items-row">Yankee Doodle Doo</div>
-            <div class="row totals-row">
+            <div class="row totals-row valign-wrapper">
                 <div class="col s8">
                     <div class="input-field coupon col s12">
                         <input class="browser-default" name="st-coupon" type="text" placeholder="Coupon code"/>
                     </div>
                 </div>
-                <div id="total" class="col s4 right-align"><span id="ttltxt">Total: $<span>0</span></span></div>
+                <div id="total" class="col s4 right-align"><span id="ttltxt">$<span>0</span></span></div>
             </div>
         </div>
         <div class="input-field col s12 l6 st-input-half-left">
