a28fb1fd2d96eab2a5ea28c06f27d9be328c3a7e
David Paul <enlightenedpie@users.noreply.github.com>
2018-07-13 13:06:17 -0700

    enenthmtmh

--- web/app/plugins/sttv/includes/functions/sttv-rest-functions.php
+++ web/app/plugins/sttv/includes/functions/sttv-rest-functions.php
@@ -33,5 +33,5 @@ function sttv_verify_web_token( WP_REST_Request $request ) {
 }
 
 function sttv_set_auth_cookie($token) {
-    setcookie('_stAuthToken',$token,time()+DAY_IN_SECONDS*7,'/','.supertutortv.com',true,true);
+    setcookie('_stAuthToken',$token,time()+DAY_IN_SECONDS*7,'/','',true,true);
 }
