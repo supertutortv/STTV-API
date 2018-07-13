ce39774c0fc6bbeda8d2b8e280e1ee66eb88d62b
David Paul <enlightenedpie@users.noreply.github.com>
2018-07-13 12:37:28 -0700

    tegwththtrh

--- web/app/plugins/sttv/includes/functions/sttv-rest-functions.php
+++ web/app/plugins/sttv/includes/functions/sttv-rest-functions.php
@@ -23,9 +23,9 @@ function sttv_rest_response( $code = '', $msg = '', $status = 200, $extra = [] )
 function sttv_verify_web_token( WP_REST_Request $request ) {
     $string = $request->get_header('Authorization') ?? $_COOKIE['_stAuthToken'] ?? '';
     $token = json_decode(json_encode(new \STTV\JWT( $string )),true);
-    if ( $token->error !== false ) return $token->error;
+    if ( $token['error'] !== false ) return $token['error'];
 
-    $pieces = explode( '|', $token->payload->sub );
+    $pieces = explode( '|', $token['payload']['sub'] );
     list( $email, $id ) = $pieces;
 
     $user = wp_set_current_user( $id );
