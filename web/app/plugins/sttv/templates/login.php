ed64536b02224d57c92c9f92c2f26c487b58bd3a
David Paul <enlightenedpie@users.noreply.github.com>
2018-07-13 18:41:39 -0700

    qtgetgwtehg

--- web/app/plugins/sttv/templates/login.php
+++ web/app/plugins/sttv/templates/login.php
@@ -1,14 +1,14 @@
 <div id="st-checkout-wrapper" class="col s12">
-    <div id="pane-1" class="st-checkout-pane row">
+    <div id="pane-1" class="st-checkout-pane row active">
         <div class="st-checkout-header col s12">
             <h2>Log in to your account!</h2>
         </div>
         <div id="st-checkout-account" class="st-checkout-form col s12 l8 push-l2">
             <div class="input-field required col s12">
-                <input class="browser-default validate email" type="email" name="st-login-username" placeholder="Email Address" required/>
+                <input class="browser-default validate email" type="email" name="st-username" placeholder="Email Address" required/>
             </div>
             <div class="input-field required col s12">
-                <input class="browser-default validate" type="password" name="st-login-password" placeholder="Password" required/>
+                <input class="browser-default validate" type="password" name="st-password" placeholder="Password" required/>
             </div>
         </div>
         <div class="st-checkout-errors col s12"></div>
