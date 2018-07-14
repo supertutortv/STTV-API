afa75e9c7c03398a2c8c02e851cb89d9355b20f0
David Paul <enlightenedpie@users.noreply.github.com>
2018-07-14 12:41:06 -0700

    rgbehnhtn

--- web/app/plugins/sttv/templates/login.php
+++ web/app/plugins/sttv/templates/login.php
@@ -1,8 +1,8 @@
 <div id="st-login-wrapper" class="col s12">
-    <div id="pane-1" class="st-login-pane st-modal-form-pane row active">
+    <div id="pane-1" class="st-login-pane row active">
         <div class="st-login-header col s12">
             <h2>Please sign into your account!</h2>
-            <span>Here's some random text to tell everyone how awesome they are and what not. <a href="#" onclick="javascript:void(0)">Need to reset your password?</a></span>
+            <span>Here's some random text to tell everyone how awesome they are and what not. <a onclick="javascript:void(0)">Need to reset your password?</a></span>
         </div>
         <div id="st-login-credentials" class="st-login-form col s12 l8 push-l2">
             <div class="input-field required col s12">
@@ -14,6 +14,20 @@
         </div>
         <div class="st-login-buttons col s12">
             <a class="st-logout-next st-logout-btn pmt-button btn waves-effect waves-light" onclick="_st.login.send()">Login</a>
+        </div>
+    </div>
+    <div id="pane-2" class="st-login-pane row">
+        <div class="st-login-header col s12">
+            <h2>Reset your password</h2>
+            <span>Here's some random text to tell everyone how awesome they are and what not. <a href="#" onclick="javascript:void(0)">Need to reset your password?</a></span>
+        </div>
+        <div id="st-login-rp-form" class="st-login-form col s12 l8 push-l2">
+            <div class="input-field required col s12">
+                <input class="browser-default validate email" type="email" name="st-rp-username" placeholder="Email Address" required/>
+            </div>
+        </div>
+        <div class="st-login-buttons col s12">
+            <a class="st-logout-next st-logout-btn pmt-button btn waves-effect waves-light" onclick="_st.login.reset()">Reset</a>
         </div>
     </div>
 </div>
