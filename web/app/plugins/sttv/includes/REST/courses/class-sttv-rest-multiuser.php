7e527d6e4911ab1eb61142b07ffc5de6632a19e5
David Paul <enlightenedpie@users.noreply.github.com>
2018-07-23 17:58:09 -0700

    ynetyn4tntn

--- web/app/plugins/sttv/includes/REST/courses/class-sttv-rest-multiuser.php
+++ web/app/plugins/sttv/includes/REST/courses/class-sttv-rest-multiuser.php
@@ -72,10 +72,10 @@ class MultiUser extends \WP_REST_Controller {
         $body = json_decode($req->get_body(),true);
         $keys = (new \STTV\Multiuser\Keys( $body[ 'user' ], $body[ 'course' ] ))->keygen( $body['qty'] );
         
-        $msg = "\r\n";
+        $msg = '<br/>';
 
         foreach ( $keys as $key ) {
-            $msg .= $key."\r\n";
+            $msg .= $key.'<br/>';
         }
 
         wp_mail(
