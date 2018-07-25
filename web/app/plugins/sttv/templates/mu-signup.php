a40e06c8f9d146184354c82d58950f5dbecd1f00
David Paul <enlightenedpie@users.noreply.github.com>
2018-07-25 11:43:29 -0700

    ybneyheythn

--- web/app/plugins/sttv/templates/mu-signup.php
+++ web/app/plugins/sttv/templates/mu-signup.php
@@ -6,19 +6,19 @@
         </div>
         <div id="st-musignup-account" class="st-musignup-form col s12 l8 push-l2">
             <div class="input-field required col s12">
-                <input class="browser-default validate" autocomplete="off" type="text" name="st-mukey" placeholder="Multi-user Key" required/>
+                <input class="browser-default validate" autocomplete="off" type="text" name="st-mukey" placeholder="Multi-user Key" onblur="_st.mu.setState(this)" required/>
             </div>
             <div class="input-field required col s12 m6 st-input-half-left">
-                <input class="browser-default validate" autocomplete="off" type="text" name="st-firstname" placeholder="First Name" required />
+                <input class="browser-default validate" autocomplete="off" type="text" name="st-firstname" placeholder="First Name" onblur="_st.mu.setState(this)" required />
             </div>
             <div class="input-field required col s12 m6 st-input-half-right">
-                <input class="browser-default validate" autocomplete="off" type="text" name="st-lastname" placeholder="Last Name" required/>
+                <input class="browser-default validate" autocomplete="off" type="text" name="st-lastname" placeholder="Last Name" onblur="_st.mu.setState(this)" required/>
             </div>
             <div class="input-field required col s12">
-                <input class="browser-default validate email" autocomplete="off" type="email" name="st-email" placeholder="Email Address" onblur="_st.checkout.setChecker(this)" required/>
+                <input class="browser-default validate email" autocomplete="off" type="email" name="st-email" placeholder="Email Address" onblur="_st.mu.setState(this)" required/>
             </div>
             <div class="input-field required col s12">
-                <input class="browser-default validate" autocomplete="off" type="password" name="st-password" placeholder="Password" required/>
+                <input class="browser-default validate" autocomplete="off" type="password" name="st-password" placeholder="Password" onblur="_st.mu.setState(this)" required/>
             </div>
         </div>
         <div class="st-musignup-buttons col s12">
