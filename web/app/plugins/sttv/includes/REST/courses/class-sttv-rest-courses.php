2bd675c1506551025c292709b2319e16615e6ba3
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-01 12:12:47 -0700

    fevwgbwgb

--- web/app/plugins/sttv/includes/REST/courses/class-sttv-rest-courses.php
+++ web/app/plugins/sttv/includes/REST/courses/class-sttv-rest-courses.php
@@ -287,6 +287,6 @@ class Courses extends \WP_REST_Controller {
 		$body = $request->get_body();
 		$file = STTV_SCRIPTS_DIR . 'python/grade.py';
 		//return "python $file $body";
-		return shell_exec("python3 $file $body");
+		print_r(shell_exec("python3 $file $body"));
 	}
 }
