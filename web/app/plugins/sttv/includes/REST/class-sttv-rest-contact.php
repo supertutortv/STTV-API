f90b7baa17bef16905fe4c8879df24fd267a9a85
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-09 12:10:20 -0700

    bnethnethnth

--- web/app/plugins/sttv/includes/REST/class-sttv-rest-contact.php
+++ web/app/plugins/sttv/includes/REST/class-sttv-rest-contact.php
@@ -70,19 +70,6 @@ class Contact extends \WP_REST_Controller {
         }
     }
 
-    public function sttv_subscribe_processor( WP_REST_Request $request ) {
-        $body = json_decode($request->get_body(),true);
-        
-        // use sttv API mailinglist function
-        $response = sttv_mailinglist_subscribe( $body['email'], $body['fname'], $body['lname'] );
-
-        if ( is_wp_error($response) ){
-            return sttv_rest_response( 'sub_error', 'There was an error subscribing you to our list. Please try again later.', 400, ['response'=>$response] );
-        } else {
-            return sttv_rest_response( 'sub_success', 'Success! Thank you for subscribing to SupertutorTV!', 200, ['response'=>$response] );
-        }
-    }
-
     public function verify_form_submit( WP_REST_Request $request ){
         $token = json_decode($request->get_body(),true);
         $token = $token['g_recaptcha_response'];
