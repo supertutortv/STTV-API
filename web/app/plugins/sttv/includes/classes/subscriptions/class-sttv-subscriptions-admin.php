75625367ea1ad0c026a7a6672443edb176aa12cb
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-18 15:17:15 -0700

    thwrthwrjr

--- web/app/plugins/sttv/includes/classes/subscriptions/class-sttv-subscriptions-admin.php
+++ web/app/plugins/sttv/includes/classes/subscriptions/class-sttv-subscriptions-admin.php
@@ -7,7 +7,7 @@ defined( 'ABSPATH' ) || exit;
 class Admin {
     public function __construct() {
         add_action('admin_init',function(){
-            add_action( 'save_post_subscriptions', [ $this, 'sttv_sub_plan' ] );
+            add_action( 'save_post_subscriptions', [ $this, 'sttv_sub_plan' ], 10, 2 );
         });
     }
 
