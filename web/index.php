d4d33afc28e0cdc7b249bf629d347d290ac69482
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-24 09:29:05 -0700

    itvkkytch

--- web/index.php
+++ web/index.php
@@ -1,9 +1,6 @@
 <?php
 
-echo json_encode(['cookie' => isset($_COOKIE['stAuth'])]);
-die;
-
-if (strpos($_SERVER['REQUEST_URI'],'/auth/verify') > -1 && !isset($_COOKIE['stAuth'])) echo json_encode(['data' => false]) && die;
+if (strpos($_SERVER['REQUEST_URI'],'/auth/verify') > -1 && !isset($_COOKIE['stAuth'])) {echo json_encode(['data' => false]); die;}
 
 /** WordPress view bootstrapper */
 define('WP_USE_THEMES', true);
