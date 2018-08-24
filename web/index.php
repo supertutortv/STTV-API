72d0d16951d02dfabf6b491a714c885e17b59d7e
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-24 09:25:14 -0700

    gfcjfhchfmchfc

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo isset($_COOKIE['stAuth']);
+echo ['cookie' => isset($_COOKIE['stAuth'])];
 die;
 
 if (strpos($_SERVER['REQUEST_URI'],'/auth/verify') > -1 && !isset($_COOKIE['stAuth'])) echo json_encode(['data' => false]) && die;
