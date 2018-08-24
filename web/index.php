dc1679d4c85bc1c20794fe7e5e6efba78d5c65d2
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-24 09:33:49 -0700

    hgdxgfhchrfc

--- web/index.php
+++ web/index.php
@@ -1,6 +1,10 @@
 <?php
 
-if (strpos($_SERVER['REQUEST_URI'],'/auth/verify') > -1 && !isset($_COOKIE['stAuth'])) {echo json_encode(['data' => false]); die;}
+if (strpos($_SERVER['REQUEST_URI'],'/auth/verify') > -1 && !isset($_COOKIE['stAuth'])) {
+    header( 'Content-Type: application/vnd.sttv.app+json' );
+    echo json_encode(['data' => false]);
+    die;
+}
 
 /** WordPress view bootstrapper */
 define('WP_USE_THEMES', true);
