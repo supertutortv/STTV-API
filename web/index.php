3f09477485336f5877c6280290a2861ed2278d38
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-24 09:14:16 -0700

    testing response times

--- web/index.php
+++ web/index.php
@@ -1,5 +1,7 @@
 <?php
 
+if (strpos($_SERVER['REQUEST_URI'],'/auth/verify') > -1 && !isset($_COOKIE['stAuth'])) echo json_encode(['data' => false]) && die();
+
 /** WordPress view bootstrapper */
 define('WP_USE_THEMES', true);
 require(__DIR__ . '/wp/wp-blog-header.php');
