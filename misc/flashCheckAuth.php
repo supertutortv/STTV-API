96da09f097272d5c1c49a765aa9690e9bae52ae2
David Paul <enlightenedpie@users.noreply.github.com>
2018-08-27 19:00:01 -0700

    wtwtrhrth

--- misc/flashCheckAuth.php
+++ misc/flashCheckAuth.php
@@ -1,5 +1,5 @@
 <?php
-if (strpos($_SERVER['REQUEST_URI'],'/auth/verify') > -1 && !isset($_COOKIE['stAuth'])) {
+if (strpos($_SERVER['REQUEST_URI'],'/auth/verify') > -1 && !isset($_COOKIE['_stAuthToken'])) {
     header( 'Access-Control-Allow-Headers: Accept, Referrer, Origin, Credentials, Content-Type, User-Agent, Access-Control-Allow-Headers, Authorization, X-RateLimit-Buster' );
     header( 'Access-Control-Allow-Origin: https://courses.supertutortv.com' );
     header( 'Access-Control-Allow-Credentials: true' );
