c60e3e2da84db22c15282a04f8a857a35e5cb4cd
David Paul <enlightenedpie@users.noreply.github.com>
2018-09-12 09:49:54 -0700

    6o67vku6tv

--- web/app/plugins/sttv/includes/REST/class-sttv-rest-auth.php
+++ web/app/plugins/sttv/includes/REST/class-sttv-rest-auth.php
@@ -187,6 +187,8 @@ class Auth extends \WP_REST_Controller {
     public function verifyPwChange( WP_REST_Request $request ) {
         list($key,$login) = $request->get_param('key');
 
+        return [$key,$login];
+
         $check = check_password_reset_key($key,$login);
         return sttv_rest_response(
             'passwordChecker',
