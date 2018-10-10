d0066875e903dff4090ed04687335e29394419a3
David Paul <enlightenedpie@users.noreply.github.com>
2018-10-10 15:22:14 -0700

    ktchfcjfhcjgfh

--- web/app/plugins/sttv/includes/REST/courses/class-sttv-rest-courses.php
+++ web/app/plugins/sttv/includes/REST/courses/class-sttv-rest-courses.php
@@ -225,7 +225,9 @@ class Courses extends \WP_REST_Controller {
 						'udata_type' => $patch,
 						'udata_timestamp' => $timestamp,
 						'udata_id' => $udata_id,
-						'udata_name' => $udata_name
+						'udata_name' => $udata_name,
+						'udata_thumb' => $udata_thumb,
+						'udata_path' => $udata_path
 					];
 					return $allowed;
 					$wpdb->insert( $wpdb->prefix.'course_udata', $allowed, ['%d','%s','%d','%s'] );
