045309c1b76257615b8fba9cbe454ad60a2377f0
David Paul <enlightenedpie@users.noreply.github.com>
2018-11-13 13:07:51 -0800

    uytkthhkrc

--- web/app/plugins/sttv/includes/classes/pricing/class-sttv-pricing-admin.php
+++ web/app/plugins/sttv/includes/classes/pricing/class-sttv-pricing-admin.php
@@ -11,16 +11,19 @@ class Admin {
                 'id' => 'SAT',
                 "name" => 'The Best SAT Prep Course Ever',
                 "type" => "service",
+                'metadata' => '[the_best_sat_prep_course_ever]'
             ]);
             \Stripe\Product::create([
                 'id' => 'ACT',
                 "name" => 'The Best ACT Prep Course Ever',
                 "type" => "service",
+                'metadata' => '[the_best_act_prep_course_ever]'
             ]);
             \Stripe\Product::create([
                 'id' => 'COMBO',
                 "name" => 'SAT and ACT Combo',
                 "type" => "service",
+                'metadata' => '[the_best_sat_prep_course_ever,the_best_act_prep_course_ever]'
             ]);
         } catch ( \Exception $e ) {}
     }
