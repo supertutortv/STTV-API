13af73633729a423c709f87b528e25f72c25bfd8
David Paul <enlightenedpie@users.noreply.github.com>
2018-11-13 14:00:04 -0800

    kcjhfxjhxf

--- web/app/plugins/sttv/includes/classes/pricing/class-sttv-pricing-admin.php
+++ web/app/plugins/sttv/includes/classes/pricing/class-sttv-pricing-admin.php
@@ -11,24 +11,42 @@ class Admin {
 
     public function makeplans() {
         try {
+            \Stripe\Product::retreive([
+                'id' => 'SAT',
+            ]);
+        } catch( \Exception $e ) {
             \Stripe\Product::create([
                 'id' => 'SAT',
                 "name" => 'The Best SAT Prep Course Ever',
                 "type" => "service",
                 'metadata' => [ 'roles' => '[the_best_sat_prep_course_ever]' ]
             ]);
+        }
+
+        try {
+            \Stripe\Product::retreive([
+                'id' => 'ACT',
+            ]);
+        } catch( \Exception $e ) {
             \Stripe\Product::create([
                 'id' => 'ACT',
                 "name" => 'The Best ACT Prep Course Ever',
                 "type" => "service",
                 'metadata' => [ 'roles' => '[the_best_act_prep_course_ever]' ]
             ]);
+        }
+
+        try {
+            \Stripe\Product::retreive([
+                'id' => 'COMBO',
+            ]);
+        } catch( \Exception $e ) {
             \Stripe\Product::create([
                 'id' => 'COMBO',
                 "name" => 'SAT and ACT Combo',
                 "type" => "service",
                 'metadata' => [ 'roles' => '[the_best_sat_prep_course_ever,the_best_act_prep_course_ever]' ]
             ]);
-        } catch ( \Exception $e ) {}
+        }
     }
 }
