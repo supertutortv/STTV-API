6383fdc3d6dd2781d7d55160ee870cd93f7de606
David Paul <enlightenedpie@users.noreply.github.com>
2018-11-15 15:56:59 -0800

    ytcjhfcjgf

--- web/app/plugins/sttv/includes/classes/courses/class-sttv-courses-admin.php
+++ web/app/plugins/sttv/includes/classes/courses/class-sttv-courses-admin.php
@@ -7,26 +7,27 @@ defined( 'ABSPATH' ) || exit;
 class Admin {
     public function __construct() {
 		add_action( 'save_post_courses', [ $this, 'sttv_build_course' ], 10, 2 );
-		add_action( 'user_register', [ $this, 'add_user_course_meta' ] );
+		add_action( 'user_register', [ $this, 'admin_course_meta' ] );
 	}
 	
-	public function add_user_course_meta( $user_id ) {
-		return update_user_meta( $user_id, 'sttv_user_data', [
-			'user' => [
-				'history' => [],
-				'downloads' => [],
-				'type' => 'standard',
-				'trialing' => false,
-				'settings' => [
-					'autoplay' => false,
-					'dark_mode' => false
+	public function admin_course_meta( $user_id ) {
+		if ( !user_can($user_id,'manage_posts') ) return false;
+			return update_user_meta( $user_id, 'sttv_user_data', [
+				'user' => [
+					'history' => [],
+					'downloads' => [],
+					'type' => 'standard',
+					'trialing' => false,
+					'settings' => [
+						'autoplay' => false,
+						'dark_mode' => false
+					],
+					'userdata' => [
+						'login_timestamps' => []
+					]
 				],
-				'userdata' => [
-					'login_timestamps' => []
-				]
-			],
-			'courses' => []
-		]);
+				'courses' => []
+			]);
 	}
     
     public function sttv_build_course( $post_id, $post ) {
