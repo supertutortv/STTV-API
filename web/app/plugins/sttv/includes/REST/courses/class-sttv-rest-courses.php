974eb29e7ba60c2830a530d36dab25ff3ce8a67b
David Paul <enlightenedpie@users.noreply.github.com>
2018-11-20 13:27:20 -0800

    tcyfcjfhjfxgh

--- web/app/plugins/sttv/includes/REST/courses/class-sttv-rest-courses.php
+++ web/app/plugins/sttv/includes/REST/courses/class-sttv-rest-courses.php
@@ -75,6 +75,7 @@ class Courses extends \WP_REST_Controller {
 	public function get_course_data( $req ) {
 		global $wpdb;
 		$userid = get_current_user_id();
+		return wp_get_current_user();
 		
 		$umeta = get_user_meta( $userid, 'sttv_user_data', true );
 
