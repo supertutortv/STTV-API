1f5418cdb010d24915faf457886658f5f2b3a0f7
David Paul <enlightenedpie@users.noreply.github.com>
2018-11-19 17:22:27 -0800

    yvuvkcgckhchfhcfc

--- web/app/plugins/sttv/includes/REST/class-sttv-rest-signup.php
+++ web/app/plugins/sttv/includes/REST/class-sttv-rest-signup.php
@@ -191,6 +191,8 @@ class Signup extends \WP_REST_Controller {
             $customer->coupon = $body['pricing']['coupon']['id'] ?: null;
             $customer->shipping = $cus['shipping'];
             $customer->save();
+
+            return $body['plan'];
             
             //Begin Order Processing
             $order = new \STTV\Checkout\Order( 'create', [
@@ -202,8 +204,7 @@ class Signup extends \WP_REST_Controller {
                 ],
                 'items' => [
                     [
-                        'plan' => $body['plan']['id'],
-                        'qty' => 1
+                        'plan' => $body['plan']['id']
                     ]
                 ]
             ]);
