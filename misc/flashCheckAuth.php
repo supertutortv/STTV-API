50e1e95b7f5b50ab1b6319428c5ab5991353aec8
David Paul <enlightenedpie@users.noreply.github.com>
2018-12-07 15:28:59 -0800

    CORS updates on flashCheckAuth

--- misc/flashCheckAuth.php
+++ misc/flashCheckAuth.php
@@ -1,5 +1,16 @@
 <?php
 if (strpos($_SERVER['REQUEST_URI'],'/auth/verify') > -1 && !isset($_COOKIE['_stAuthToken'])) {
+    $allowed_origins = [
+        'https://supertutortv.com',
+        //'https://dev.courses.supertutortv.com',
+        'https://courses.supertutortv.com',
+        'https://api.supertutortv.com'
+    ];
+    if ( in_array( $_SERVER['HTTP_ORIGIN'], $allowed_origins ) ) {
+        header( 'Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN'] );
+    } else {
+        header( 'Access-Control-Allow-Origin: X' );
+    }
     header( 'Access-Control-Allow-Headers: Accept, Referrer, Origin, Credentials, Content-Type, User-Agent, Access-Control-Allow-Headers, Authorization, X-RateLimit-Buster' );
     header( 'Access-Control-Allow-Origin: https://courses.supertutortv.com' );
     header( 'Access-Control-Allow-Credentials: true' );
