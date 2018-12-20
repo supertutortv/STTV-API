3451b16b467766ae53c968ec52d52c9d34d646b9
David Paul <enlightenedpie@users.noreply.github.com>
2018-12-20 14:55:35 -0800

    kutcjfhjgfxjxgf

--- web/app/plugins/sttv/includes/REST/class-sttv-rest-migrate.php
+++ web/app/plugins/sttv/includes/REST/class-sttv-rest-migrate.php
@@ -43,7 +43,7 @@ class Migrate extends \WP_REST_Controller {
 
     public function users( WP_REST_Request $request ) {
         $body = unserialize($request->get_body());
-        unset($body->ID);
+        unset($body->data->ID);
         return sttv_rest_response(
             'migration',
             'Migration successful',
