ad04e9409fa093a50b0a632724bd7f6159afd4c5
David Paul <enlightenedpie@users.noreply.github.com>
2018-12-26 12:30:40 -0800

    notifications REST endpoints

---
+++ web/app/plugins/sttv/includes/REST/courses/class-sttv-rest-notifications.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace STTV\REST\Courses;
+
+defined( 'ABSPATH' ) || exit;
+
+class Notifications extends \WP_REST_Controller {
+	
+	public function __construct() {}
+
+	public function register_routes() {
+		$routes = [
+			'/notifications' => [
+				[
+					'methods' => 'GET',
+					'callback' => [ $this, 'retrieve' ],
+					'permission_callback' => 'sttv_verify_web_token'
+				],
+				[
+					'methods' => 'PUT',
+					'callback' => [ $this, 'add' ],
+					'permission_callback' => 'sttv_verify_web_token'
+				]
+			]
+		];
+
+		foreach ( $routes as $route => $endpoint ) {
+			register_rest_route( 'courses', $route, $endpoint );
+		}
+	}
+
+	public function retrieve(WP_REST_Request $req) {
+		$u = wp_get_current_user();
+		$notin = get_user_meta( $u->ID, 'cn_dismissed', true ) ?: [];
+		return get_posts(['post_type'=>'notifications','post__not_in'=>$notin]);
+	}
+
+	public function add(WP_REST_Request $req) {
+		$u = wp_get_current_user();
+		$notin = get_user_meta( $u->ID, 'cn_dismissed', true ) ?: [];
+		list($id) = json_decode($req->get_body(),true);
+		return $notin[$id];
+	}
+}
