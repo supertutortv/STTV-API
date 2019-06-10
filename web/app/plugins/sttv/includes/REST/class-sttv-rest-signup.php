b66c3163081bdffb1650bae484f17dafef2e54fc
David Paul <enlightenedpie@gmail.com>
2019-06-09 21:53:14 -0500

    check check

--- web/app/plugins/sttv/includes/REST/class-sttv-rest-signup.php
+++ web/app/plugins/sttv/includes/REST/class-sttv-rest-signup.php
@@ -187,7 +187,12 @@ class Signup extends \WP_REST_Controller {
 
             $cus = \Stripe\Customer::update('cus_'.$user->user_login,$edits);
 
-            return json_decode(get_option('pricingplan_'.$plan['id']));
+            $thePlan = json_decode(get_option('pricingplan_'.$plan['id']));
+
+            for ($i = 0, $size = count($thePlan['plans']); $i < $size; $i++) {
+                $refarr = ['month'=>6,'year'=>12];
+                if ($refarr[$thePlan['plans'][$i]['interval']] === $plan['length']) $thePlan['plans'][$i];
+            }
 
             /* $order = \Stripe\Subscription::create([
                 'customer' => $cus->id,
