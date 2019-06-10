6cb6c0528fb293786e5fd8c14364089fedac8bc1
David Paul <enlightenedpie@gmail.com>
2019-06-09 19:16:44 -0500

    check check

--- web/app/plugins/sttv/includes/REST/class-sttv-rest-checkout.php
+++ web/app/plugins/sttv/includes/REST/class-sttv-rest-checkout.php
@@ -118,6 +118,8 @@ class Checkout extends \WP_REST_Controller {
     private function _pay( $body, $request ) {
         if ( empty($body) ) return sttv_rest_response( 'signupError', 'Request body cannot be empty', 200 );
 
+        return wp_get_current_user();
+
         return sttv_stripe_errors(function() use ($body) {
             $customer = $create_invoice = $cid = $login = $items = $user = $plan = false;
             $items = $courseids = [];
