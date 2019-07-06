39137c6e9fcce87db48eeead46f4112ae7e0c874
David Paul <enlightenedpie@gmail.com>
2019-07-06 13:33:09 -0500

    devpush - fixing coupon checker

--- web/app/plugins/sttv/includes/REST/class-sttv-rest-signup.php
+++ web/app/plugins/sttv/includes/REST/class-sttv-rest-signup.php
@@ -285,10 +285,8 @@ class Signup extends \WP_REST_Controller {
     private function check_coupon( $coupon, $sig ) {
         if ( empty( $coupon ) ) return sttv_rest_response( 'bad_request', 'Coupon cannot be empty or blank.', 400 );
         try {
-            $coupon = \Stripe\Coupon::retrieve( $coupon );
-            if ( !$coupon->valid ) return sttv_rest_response( 'signupError', 'Expired coupon', 200 );
-
-            $coupon = (array) $coupon;
+            $coupon = (array) \Stripe\Coupon::retrieve( $coupon );
+            //if ( !$coupon->valid ) return sttv_rest_response( 'signupError', 'Expired coupon', 200 );
 
             return sttv_rest_response(
                 'coupon_valid',
